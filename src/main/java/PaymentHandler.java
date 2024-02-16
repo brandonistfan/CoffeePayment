5a12f2da00463335c675406bc8d62f6e071c28c0
Brandon Istfan <brandonistfan@gmail.com>
2024-02-16 00:11:34 -0500

    Java Version Update Commit

--- src/main/java/PaymentHandler.java
+++ src/main/java/PaymentHandler.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class PaymentHandler {
+import java.util.Comparator;
+import java.util.List;
+
+public class PaymentHandler {
+    public Customer getNextPayer(List<Customer> customers) {
+        return customers.stream()
+                .min(Comparator.comparingDouble(Customer::getTotalPaid))
+                .orElse(null);
+    }
 }
